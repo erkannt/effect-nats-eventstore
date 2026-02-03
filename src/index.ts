a5906847b2f2267bb984dc7bd56c89734e76bb26
Daniel Haarhoff <daniel@rknt.de>
2026-02-03 16:35:42 +0000

    Try using HttpApi Module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
-import { NodeRuntime } from "@effect/platform-node";
-import { Effect, Console } from "effect";
+import {
+  HttpApi,
+  HttpApiBuilder,
+  HttpApiEndpoint,
+  HttpApiGroup,
+  HttpServerResponse,
+} from "@effect/platform";
+import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
+import { Effect, pipe, Schema, Layer } from "effect";
+import { createServer } from "node:http";
 
-const program = Console.log("Hello, World!");
+const UserFacingRoutes = HttpApiGroup.make("UserFacing").add(
+  HttpApiEndpoint.get("landing")`/`.addSuccess(Schema.String),
+);
+const MainApi = HttpApi.make("MainApi").add(UserFacingRoutes);
 
-NodeRuntime.runMain(program);
+const UserFacingApiGroupImplementation = HttpApiBuilder.group(
+  MainApi,
+  "UserFacing",
+  (handlers) =>
+    handlers.handle("landing", () =>
+      Effect.succeed(HttpServerResponse.html("Hello, world")),
+    ),
+);
+
+const MainApiImplementation = pipe(
+  HttpApiBuilder.api(MainApi),
+  Layer.provide(UserFacingApiGroupImplementation),
+);
+
+const server = pipe(
+  HttpApiBuilder.serve(),
+  Layer.provide(MainApiImplementation),
+  Layer.provide(NodeHttpServer.layer(createServer, { port: 8080 })),
+  Layer.launch,
+);
+
+NodeRuntime.runMain(server);
