f06282b5b7430964e27d8d37f845da7f7a2ca322
Daniel Haarhoff <daniel@rknt.de>
2026-02-06 12:00:05 +0000

    Simplify by using HttpServer and HttpRouter instead of HttpApi

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,16 @@
-import {
-  HttpApi,
-  HttpApiBuilder,
-  HttpApiEndpoint,
-  HttpApiGroup,
-  HttpServerResponse,
-} from "@effect/platform";
+import { HttpRouter, HttpServer, HttpServerResponse } from "@effect/platform";
 import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
-import { Effect, pipe, Schema, Layer } from "effect";
+import { Layer, pipe } from "effect";
 import { createServer } from "node:http";
 
-const UserFacingRoutes = HttpApiGroup.make("UserFacing").add(
-  HttpApiEndpoint.get("landing")`/`.addSuccess(Schema.String),
-);
-const MainApi = HttpApi.make("MainApi").add(UserFacingRoutes);
-
-const UserFacingApiGroupImplementation = HttpApiBuilder.group(
-  MainApi,
-  "UserFacing",
-  (handlers) =>
-    handlers.handle("landing", () =>
-      Effect.succeed(HttpServerResponse.html("Hello, world!")),
-    ),
-);
-
-const MainApiImplementation = pipe(
-  HttpApiBuilder.api(MainApi),
-  Layer.provide(UserFacingApiGroupImplementation),
+const Router = pipe(
+  HttpRouter.empty,
+  HttpRouter.get("/", HttpServerResponse.html("Hello, world!")),
 );
 
 const server = pipe(
-  HttpApiBuilder.serve(),
-  Layer.provide(MainApiImplementation),
+  Router,
+  HttpServer.serve(),
   Layer.provide(NodeHttpServer.layer(createServer, { port: 8080 })),
   Layer.launch,
 );
