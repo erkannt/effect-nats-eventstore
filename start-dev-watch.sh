bbf87b119c3d598b37da42b1607fdbc2ba71207f
Daniel Haarhoff <daniel@rknt.de>
2026-02-10 09:08:00 +0000

    Improve watching

--- start-dev-watch.sh
+++ start-dev-watch.sh
@@ -2,10 +2,10 @@
 
 rm -f static/style.css static/style.css.map
 
-node_modules/.bin/tsc --noCheck --outDir dist src/index.ts &
+node_modules/.bin/tsc --watch --preserveWatchOutput --noCheck --outDir dist src/index.ts &
 pid1=$!
 
-node --watch dist/index.js &
+node --watch --watch-preserve-output dist/index.js &
 pid2=$!
 
 trap "echo 'Caught signal; killing watchers'; kill ${pid1} ${pid2}; exit 1" INT QUIT TERM
