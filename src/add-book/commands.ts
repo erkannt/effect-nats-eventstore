450fd3763a1532bee5fc9d9dbc642c4935e2c6b5
Daniel Haarhoff <daniel@rknt.de>
2026-03-19 18:36:18 +0000

    Extract and name parts of Service construction for legibility

--- src/add-book/commands.ts
+++ src/add-book/commands.ts
@@ -1,21 +1,20 @@
 import { Effect, Layer, ServiceMap } from "effect";
 
-export class Commands extends ServiceMap.Service<
-  Commands,
-  {
-    readonly started: (params: {
-      bookTitle: string;
-      bookAuthor: string;
-    }) => void;
-  }
->()("Commands", {
-  make: Effect.gen(function* () {
-    return {
-      started: (input) => {
-        console.log("received 'started' command", input);
-      },
-    };
-  }),
-}) {
+type Methods = {
+  readonly started: (params: { bookTitle: string; bookAuthor: string }) => void;
+};
+
+const make: Effect.Effect<Methods> = Effect.gen(function* () {
+  return {
+    started: (input) => {
+      console.log("received 'started' command", input);
+    },
+  };
+});
+
+export class Commands extends ServiceMap.Service<Commands, Methods>()(
+  "Commands",
+  { make },
+) {
   static readonly layer = Layer.effect(this, this.make);
 }
